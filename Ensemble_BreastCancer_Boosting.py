3e43aabb848c31d4efa0ac748d0ed3175c04165d
Swaraj Jagtap <swarajjagtap22@gmail.com>
2026-08-26 20:56:32 +0530

    Add files via upload

---
+++ Ensemble_BreastCancer_Boosting.py
@@ -0,0 +1,77 @@
+import pandas as pd
+
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix,accuracy_score,classification_report
+
+
+#----------------------------------------
+#Step 1 : Load The dataset
+#----------------------------------------
+
+df = pd.read_csv("breast_cancer.csv")
+
+print("Shape Of dataset",df.shape)
+print("First 5 Records",df.head())
+
+#----------------------------------------
+#Step 2 : Separate Features and Labels 
+#----------------------------------------
+
+X = df.drop("target", axis=1)
+
+Y = df["target"]
+
+print("X Shape",X.shape)
+print("Y Shape",Y.shape)
+
+#--------------------------------------------------
+#Step 3 : Split dataset for training and testing 
+#-------------------------------------------------
+
+X_train , X_test , Y_train , Y_test = train_test_split(
+                                                X,
+                                                Y,
+                                                test_size=0.2,
+                                                random_state=42
+                                                )
+
+#--------------------------------------------------
+#Step 4 : Scale the Features
+#-------------------------------------------------
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+#--------------------------------------------------
+#Step 5 : Create The Boosting Model
+#-------------------------------------------------
+
+model = AdaBoostClassifier(
+    n_estimators=50,
+    learning_rate=1,
+    random_state=42
+                           )
+
+#--------------------------------------------------
+#Step 6 : Train The Model
+#-------------------------------------------------
+
+model = model.fit(X_train,Y_train)
+
+#--------------------------------------------------
+#Step 7 : Test The Model
+#-------------------------------------------------
+
+Y_pred = model.predict(X_test)
+
+#--------------------------------------------------
+#Step 8 : Evaluate The Model
+#-------------------------------------------------
+
+print("Accuracy :",accuracy_score(Y_test,Y_pred))
+print("Confusion Matrix :",confusion_matrix(Y_test,Y_pred))
+
